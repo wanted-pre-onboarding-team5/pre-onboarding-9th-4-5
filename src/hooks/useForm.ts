60222a0ef592cb312fa3a75eb3633807ca36dbcc
eunoo1995 <eunoo1995@gmail.com>
2023-03-20 18:56:05 +0900

    [#6] Feat: form 제어 로직 useForm 커스텀 훅 구현
    useSearchParams 훅을 활용한 필터 및 검색에 사용될 커스텀 훅 구현

---
+++ src/hooks/useForm.ts
@@ -0,0 +1,15 @@
+import { SelectChangeEvent } from '@mui/material';
+import queryString from 'query-string';
+import { useSearchParams } from 'react-router-dom';
+
+export const useForm = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const params = queryString.parse(searchParams.toString());
+
+  const handleChange = (e: SelectChangeEvent) => {
+    const newParams = { ...params, status: e.target.value };
+    setSearchParams(queryString.stringify(newParams));
+  };
+
+  return { status: params.status || 'all', handleChange };
+};
