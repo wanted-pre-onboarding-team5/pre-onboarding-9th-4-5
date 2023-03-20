0e20b8ab23ad38d1a8b8bf272fe19b9b41b5e2c5
Jayun <jay.lee.2280@gmail.com>
2023-03-20 16:20:32 +0900

    [#2] Design: title 및 Header 이름 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Typography } from '@mui/material';
 export const Header = () => {
   return (
     <>
-      <Typography variant='h4'>Switchwon Order Table</Typography>
+      <Typography variant='h4'>Switchwon</Typography>
     </>
   );
 };
