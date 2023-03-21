4f31b9f0aab125c3255ef0f55cd0d6bb0847c9fe
letsjo <hyunoh.jo@gmail.com>
2023-03-22 03:53:44 +0900

    [#7] Refactor: fetchOrder Filter 관련 로직 분리

--- src/helpers/fetchOrder.ts
+++ src/helpers/fetchOrder.ts
@@ -1,5 +1,7 @@
 import { API_PATH } from '@/constants';
 
+import { filterOrderList } from './filterOrderList';
+
 import { SwitchOneApi } from '@/types/responseData';
 import { httpClient } from '@/utils/httpClient';
 
@@ -7,32 +9,4 @@ export const fetchOrder = async (filters: SwitchOneApi) => {
   const orderList: SwitchOneApi[] = await httpClient(API_PATH.data);
   const filteredOrderList = filterOrderList(orderList, filters);
   return filteredOrderList;
-};
-
-const filterOrderList = (orderList: SwitchOneApi[], filters: SwitchOneApi) => {
-  return orderList.reduce((acc, order) => {
-    const idCondition = filters.id ? order.id === filters.id : true;
-    const timeCondition = filters.transaction_time
-      ? order.transaction_time?.includes(filters.transaction_time)
-      : true;
-    const statusCondition =
-      filters.status !== 'all' ? String(order.status) === filters.status : true;
-    const customerIdCondition = filters.customer_id
-      ? order.customer_id === filters.customer_id
-      : true;
-    const customerNameCondition = filters.customer_name
-      ? order.customer_name?.includes(filters.customer_name)
-      : true;
-
-    if (
-      idCondition &&
-      timeCondition &&
-      statusCondition &&
-      customerIdCondition &&
-      customerNameCondition
-    ) {
-      acc.push(order);
-    }
-    return acc;
-  }, []);
 };
