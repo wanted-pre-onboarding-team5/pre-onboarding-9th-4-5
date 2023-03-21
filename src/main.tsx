0101497af3a2a085fe573318ff9a8c335aa28ccc
junsu1220 <crab7157@gmail.com>
2023-03-21 12:38:37 +0900

    [#4] Feat: TablePagination 및 main에 QueryClientProvider 추가
    OrderTable로 부터 전체 개수를 props로 받고
    mui icon과 button을 이용해 page를 바꿉니다.
    이때 useSearchParams를 이용해 query를 수정해주는 방식을 이용합니다.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,25 @@
+import { CssBaseline } from '@mui/material';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from '@/App';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
+
 const rootElement = document.getElementById('root') as HTMLElement;
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <CssBaseline />
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
