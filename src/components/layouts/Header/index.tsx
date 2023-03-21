55c1713df9af0b637be466b7a1cea2f631721845
letsjo <hyunoh.jo@gmail.com>
2023-03-21 15:38:08 +0900

    [#2] Refactor: Header Logo 추가 및 Link

--- src/components/layouts/Header/index.tsx
+++ src/components/layouts/Header/index.tsx
@@ -4,7 +4,12 @@ import React from 'react';
 const Header = () => {
   return (
     <Container>
-      <h1>SWITCH ONE</h1>
+      <h1>
+        <a href='/'>
+          <img style={{ width: '200px' }} src='/logo.png' alt='logo' />
+          <span style={{ display: 'none' }}>SWITCH ONE</span>
+        </a>
+      </h1>
     </Container>
   );
 };
