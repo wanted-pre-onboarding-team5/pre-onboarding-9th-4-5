b605b9fc66e11fe85764800a2d251daa6ec96fd9
letsjo <hyunoh.jo@gmail.com>
2023-03-21 12:12:58 +0900

    [#6] Feat: data filter 기능 추가

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import { Box } from '@mui/material';
+import { useState } from 'react';
 
-const Order = () => {
-  return <div>Order</div>;
+import { HEAD_CELLS, FILTER_STATUS } from '@/constants';
+
+import DataTable, { TableOption } from '@/components/public/DataTable';
+import FilterRadios from '@/components/public/FilterRadios';
+import Spinner from '@/components/public/Spinner';
+import { useFetchOrder } from '@/hooks/useFetchOrder';
+import { getToday } from '@/utils/getToday';
+
+interface SwitchOneApi {
+  id?: number;
+  transaction_time?: string;
+  status?: boolean;
+  customer_id?: number;
+  customer_name?: string;
+  currency?: string;
+}
+
+const tableOption: TableOption = {
+  defaultOrder: 'asc',
+  defaultOrderBy: 'id',
+  headerCells: HEAD_CELLS,
 };
 
-export default Order;
+export const Orders = () => {
+  const todayDate = getToday();
+  const [filters, setFilters] = useState({ transaction_time: todayDate, status: 'all' });
+
+  const handleStatus = (event: React.MouseEvent<HTMLElement>, status: string) => {
+    setFilters({ ...filters, status });
+  };
+
+  const {
+    data,
+    isLoading,
+  }: {
+    data: SwitchOneApi[] | undefined;
+    isLoading: boolean;
+    error: boolean | null;
+  } = useFetchOrder(filters);
+
+  return (
+    <Box sx={{ height: 800 }}>
+      <FilterRadios radios={FILTER_STATUS} filters={filters} handleStatus={handleStatus} />
+      {isLoading ? <Spinner /> : <DataTable tableDataList={data} tableOption={tableOption} />}
+    </Box>
+  );
+};
