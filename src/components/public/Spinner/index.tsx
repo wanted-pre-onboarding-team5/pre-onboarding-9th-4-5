d1d0d833eb07fd9b62126cd4f207c543e2715551
letsjo <hyunoh.jo@gmail.com>
2023-03-21 20:00:39 +0900

    Feat: Spinner 색 추가

--- src/components/public/Spinner/index.tsx
+++ src/components/public/Spinner/index.tsx
@@ -3,8 +3,35 @@ import React from 'react';
 
 const Spinner = () => {
   return (
-    <Box sx={{ display: 'flex', width: '100%', justifyContent: 'center' }}>
-      <CircularProgress size='100px' />
+    <Box
+      sx={{
+        position: 'relative',
+        display: 'flex',
+        width: '100%',
+        justifyContent: 'center',
+        paddingTop: 15,
+      }}
+    >
+      <CircularProgress
+        variant='determinate'
+        sx={{
+          color: (theme) => theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+        }}
+        size={100}
+        thickness={4}
+        value={100}
+      />
+      <CircularProgress
+        variant='indeterminate'
+        disableShrink
+        sx={{
+          color: (theme) => (theme.palette.mode === 'light' ? '#F7941C' : '#d28d38'),
+          animationDuration: '550ms',
+          position: 'absolute',
+        }}
+        size={100}
+        thickness={4}
+      />
     </Box>
   );
 };
