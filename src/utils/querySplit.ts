4409e88b79bd18d6763973d696435db827a26cdc
letsjo <hyunoh.jo@gmail.com>
2023-03-22 00:54:25 +0900

    [#2] Feat: 매개변수 반환값 Type 선언

--- src/utils/querySplit.ts
+++ src/utils/querySplit.ts
@@ -1,4 +1,4 @@
-export const querySplit = (queries) => {
+export const querySplit = (queries: string): { [key: string]: string } => {
   return queries.split('&').reduce((acc, query) => {
     const [key, value] = query.split('=');
     acc[key] = value;
