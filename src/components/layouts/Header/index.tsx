ecb2564dbd49447082d86bd1b13d08b0af917c5b
letsjo <hyunoh.jo@gmail.com>
2023-03-21 11:56:38 +0900

    [#4] Feat: init Header layout

---
+++ src/components/layouts/Header/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from '@mui/material';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <Container>
+      <h1>SWITCH ONE</h1>
+    </Container>
+  );
+};
+
+export default Header;
