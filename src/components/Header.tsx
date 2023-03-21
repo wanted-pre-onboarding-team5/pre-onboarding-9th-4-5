acf18d927a50466d3a79e57f274b208013521a48
Jayun <jay.lee.2280@gmail.com>
2023-03-21 10:31:03 +0900

    [#2] Design: logo, Header 디자인 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,16 @@
-import { Typography } from '@mui/material';
+import { Box } from '@mui/material';
 
 export const Header = () => {
   return (
-    <>
-      <Typography variant='h4'>Switchwon</Typography>
-    </>
+    <Box
+      padding='1em'
+      marginBottom='10px'
+      sx={{
+        backgroundColor: 'grey.100',
+      }}
+    >
+      <img src='public/switchone-logo.png' width='200px' />
+    </Box>
   );
 };
 4;
