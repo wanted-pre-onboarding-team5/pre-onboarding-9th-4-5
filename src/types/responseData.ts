7565feda0a394b738d84d5b35b41a5a7895ee134
Seok-CH <tjr6677@gmail.com>
2023-03-23 18:08:47 +0900

    [#23] Remove: 안쓰는 데이터 타입 삭제
    - 로직 변경으로 TransformedData를 안쓰게 되어 삭제하였습니다
    Co-authored-by: seunghoonKang <seunghoonKang@users.noreply.github.com>
    Co-authored-by: junsu park <junsu1220@users.noreply.github.com>
    Co-authored-by: hannaax <hannaax@users.noreply.github.com>
    Co-authored-by: Tummy <letsjo@users.noreply.github.com>
    Co-authored-by: Jayun Lee <21bm30@gmail.com>
    Co-authored-by: eunoo <eunoo1995@users.noreply.github.com>

--- src/types/responseData.ts
+++ src/types/responseData.ts
@@ -7,12 +7,3 @@ export type ResponseObject = {
   customer_name: string;
   currency: string;
 };
-
-export type TransformedData = TransformedObject[];
-export type TransformedObject = {
-  id: number;
-  customer: { id: number; name: string };
-  status: 'approved' | 'rejected';
-  currency: string;
-  datetime: string;
-};
