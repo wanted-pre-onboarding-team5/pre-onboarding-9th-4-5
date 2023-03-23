9c573c9a1354e6f52802938a50618e136d9760d0
Seok-CH <tjr6677@gmail.com>
2023-03-23 14:43:25 +0900

    [#23] Feat: 헤더에 로고 추가 및 파비콘 추가
    Co-authored-by: seunghoonKang <seunghoonKang@users.noreply.github.com>
    Co-authored-by: junsu park <junsu1220@users.noreply.github.com>
    Co-authored-by: hannaax <hannaax@users.noreply.github.com>
    Co-authored-by: Tummy <letsjo@users.noreply.github.com>
    Co-authored-by: Jayun Lee <21bm30@gmail.com>
    Co-authored-by: eunoo <eunoo1995@users.noreply.github.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import { Container, Typography, Box } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Container maxWidth='md' sx={{ marginTop: 2 }}>
+      <Link to='/'>
+        <Box
+          component='img'
+          style={{ width: '120px' }}
+          src='/assets/switchone_logo.png'
+          alt='SWITCH WON'
+        />
+        <Typography component='h1' style={{ display: 'none' }}>
+          SWITCH WON
+        </Typography>
+      </Link>
+    </Container>
+  );
 };
 
 export default Header;
